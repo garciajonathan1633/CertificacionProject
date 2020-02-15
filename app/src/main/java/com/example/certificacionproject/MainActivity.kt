8072fa1542574545330bf6fac99a8af05b09ed00
Jonathan Garcia <jonathan.garcia@insulet.com>
2020-02-15 12:48:51 -0800

    Removed code from toast

--- app/src/main/java/com/example/certificacionproject/MainActivity.kt
+++ app/src/main/java/com/example/certificacionproject/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.certificacionproject
 
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonToast.setOnClickListener {
-            val inflater = layoutInflater
-            val layout = inflater.inflate(R.layout.custom_toast, custom_toast_container)
-            val text: TextView = layout.findViewById(R.id.text)
-            text.text = "This is a custom toast"
-            with (Toast(applicationContext)) {
-                setGravity(Gravity.TOP, 0, 0)
-                duration = Toast.LENGTH_LONG
-                view = layout
-                show()
-            }
-        }
-
-        val text = "Hello toast!"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(applicationContext, text, duration)
-
-        with(toast){
-            setGravity(Gravity.TOP or Gravity.START, 0, 0)
-            show()
-        }
     }
 }
